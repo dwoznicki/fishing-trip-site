ccf8c42194f133b96dab68b595ed4f1bb4e1e6f3
Daniel Woznicki <tokay89@sbcglobal.net>
2015-12-12 14:42:31 -0800

    Add beginnings of front page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  changePicOnScroll();
 });
+function changePicOnScroll() {
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() >= $('.background-pictures').offset().top + $('.background-pictures').outerHeight() - window.innerHeight) {
+      console.log("End reached")
+    };
+  });
+}
